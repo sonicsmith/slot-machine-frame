b30e08393683037a38b808d5bf8f1b1085c3db16
Nic Smith <thesonicsmith@gmail.com>
2024-05-09 20:05:34 +1200

    Fix RNG

--- app/frames/outcome/route.tsx
+++ app/frames/outcome/route.tsx
@@ -2,7 +2,7 @@ import { frames } from "@/app/frames";
 import { appURL } from "@/app/utils";
 import { Button } from "frames.js/next";
 
-const NUMBER_OF_OUTCOMES = 14;
+const NUMBER_OF_OUTCOMES = 15;
 
 export const POST = frames(async (ctx: any) => {
   // ctx.message.requesterCustodyAddress
@@ -10,14 +10,14 @@ export const POST = frames(async (ctx: any) => {
   // ctx.clientProtocol.id
   const baseUrl = appURL();
 
-  const randomIndex = Math.floor(Math.random() * NUMBER_OF_OUTCOMES) - 1;
+  const randomIndex = Math.floor(Math.random() * NUMBER_OF_OUTCOMES);
 
   const isWinner = randomIndex === 0;
 
   return {
     image: `${baseUrl}/images/slotMachine/${randomIndex}.png`,
     buttons: [
-      <Button action="post" key="1" target={isWinner ? "/claim" : "/"}>
+      <Button action="post" key="1" target={isWinner ? "/claim" : "/spinning"}>
         {isWinner ? "View your prize!" : "Try again"}
       </Button>,
     ],
